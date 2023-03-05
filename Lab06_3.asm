b9d197ed66a619774c4b6a7a9174f4bfcfd7d1bb
Brodie <cwoollas@gmail.com>
2023-03-05 17:45:00 -0500

    Add files via upload

--- Lab06_3.asm
+++ Lab06_3.asm
@@ -14,16 +14,15 @@ TH2 	 DATA 0CDh
 
 org 0
 	
-		setb p1.0			;initialize p1.0 for input, counts customer
-		setb p1.1			;initialize p1.1 for input, trigger capture
-		mov t2con,#00001011b;initialize timer2 con, EXEN HIGH, C/T2 HIGH, CP HIGH
+		setb p1.0				;initialize p1.0 for input, counts customer
+		setb p1.1				;initialize p1.1 for input, trigger capture
+		mov t2con,#00001011b	;initialize timer2 con, EXEN HIGH, C/T2 HIGH, CP HIGH
 		mov TL2,#0
 		mov TH2,#0
 	
-wait1:	jnb p1.1,wait1
-wait2:	jb p1.1,wait2
-		acall rstTimer2
-		sjmp wait1
+wait1:	jnb exf2,wait1			;wait on timer 2 capture flag
+		acall Ato7seg
+		sjmp wait1				;repeates for consecutive days after rest and displaying 7seg
 	sjmp $
 
 ;*********************************************************************
@@ -33,15 +32,15 @@ wait2:	jb p1.1,wait2
 ;Outputs: Acc
 ;Destroys: Acc,tl2,th2,exf2
 ;*********************************************************************
-rstTimer2:
+Ato7seg:
 		push psw
-		mov tl2,#0
+		mov tl2,#0				;clear both timer2 bytes
 		mov th2,#0
-		clr exf2
-		mov a,rcap2l
-		anl a,#0Fh
-		acall lookup
-		cpl a
+		clr exf2				;reset the flag for next day
+		mov a,rcap2l			;mov low count from day into accumulator
+		anl a,#0Fh				;remove high nibble
+		acall hex2sevenSeg
+		cpl a					;convert to active low
 		mov p0,a
 		pop psw
 		ret
@@ -51,9 +50,9 @@ rstTimer2:
 ;Outputs: Acc
 ;Destroys: Acc
 ;*********************************************************************
-lookup:	inc a
-		movc a,@a+pc
-		ret
+hex2sevenSeg:	inc a
+				movc a,@a+pc
+				ret
 svnseg:	db 3fh,06h,5bh,4fh,66h,6dh,7dh,07h,7fh,4fh,77h,7bh,39h,5eh,79h,71h
 	
 end
